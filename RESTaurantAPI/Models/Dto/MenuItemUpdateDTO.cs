294f057ff1f524069cb38f13c63df4fd5337dd39
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-12 23:26:26 +0100

    Create model dtos for create and update menu item api endpoints

---
+++ RESTaurantAPI/Models/Dto/MenuItemUpdateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RESTaurantAPI.Models.Dto
+{
+    public class MenuItemUpdateDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public string? SpecialTag { get; set; }
+        public string? Category { get; set; }
+        [Range(1, int.MaxValue)]
+        public double Price { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
