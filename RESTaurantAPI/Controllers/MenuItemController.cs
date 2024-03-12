c0377259b96bb9a5f71f70125baf9cabc3ad3aec
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-12 22:09:03 +0100

    Create get endpoint for retrieving all menu items

---
+++ RESTaurantAPI/Controllers/MenuItemController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTaurantAPI.Data;
+using RESTaurantAPI.Models;
+using System.Net;
+
+namespace RESTaurantAPI.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMenuItems()
+        {
+            _response.Result = _db.MenuItems;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
