4dcf8facc8e01b0c9138102e5dd318029d2cd0d3
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-12 22:26:50 +0100

    Add interface and implementation for blob service to read/update/delete images from azure container

---
+++ RESTaurantAPI/Services/BlobService.cs
@@ -0,0 +1,21 @@
+﻿
+namespace RESTaurantAPI.Services
+{
+    public class BlobService : IBlobService
+    {
+        public Task<string> DeleteBlob(string blobName, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetBlob(string blobName, string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UploadBlob(string blobName, string containerName, IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
