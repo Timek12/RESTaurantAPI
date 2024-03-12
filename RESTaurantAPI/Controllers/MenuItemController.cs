f09cb25d70f145300ec028d47cf97fcdac20da7d
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-12 23:27:05 +0100

    Add blob service using dependency injection, implement an endpoint for creating new menu item

--- RESTaurantAPI/Controllers/MenuItemController.cs
+++ RESTaurantAPI/Controllers/MenuItemController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using RESTaurantAPI.Data;
 using RESTaurantAPI.Models;
+using RESTaurantAPI.Models.Dto;
+using RESTaurantAPI.Services;
+using RESTaurantAPI.Utility;
 using System.Net;
 
 namespace RESTaurantAPI.Controllers
@@ -11,10 +14,12 @@ namespace RESTaurantAPI.Controllers
     public class MenuItemController : ControllerBase
     {
         private readonly ApplicationDbContext _db;
+        private readonly IBlobService _blobService;
         private ApiResponse _response;
-        public MenuItemController(ApplicationDbContext db)
+        public MenuItemController(ApplicationDbContext db, IBlobService blobService)
         {
             _db = db;
+            _blobService = blobService;
             _response = new ApiResponse();
         }
 
@@ -26,7 +31,7 @@ namespace RESTaurantAPI.Controllers
             return Ok(_response);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name= "GetMenuItem")]
         public async Task<IActionResult> GetMenuItem(int id)
         {
             if(id == 0)
@@ -48,5 +53,54 @@ namespace RESTaurantAPI.Controllers
             _response.StatusCode = HttpStatusCode.OK;
             return Ok(_response);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> CreateMenuItem([FromForm] MenuItemCreateDTO menuItemCreateDTO)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    if(menuItemCreateDTO.File is null || menuItemCreateDTO.File.Length <= 0)
+                    {
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        _response.Errors.Add("File is required");
+                        return BadRequest(_response);
+                    }
+
+                    string fileName = $"{Guid.NewGuid()}{Path.GetExtension(menuItemCreateDTO.File.FileName)}";
+
+                    // here better approach would be to use automapper
+                    MenuItem menuItemToCreate = new()
+                    {
+                        Name = menuItemCreateDTO.Name,
+                        Price = menuItemCreateDTO.Price,
+                        Category = menuItemCreateDTO.Category,
+                        SpecialTag = menuItemCreateDTO.SpecialTag,
+                        Description = menuItemCreateDTO.Description,
+                        Image = await _blobService.UploadBlob(fileName, SD.SD_Storage_Container, menuItemCreateDTO.File)
+                    };
+                    _db.MenuItems.Add(menuItemToCreate);
+                    await _db.SaveChangesAsync();
+                    _response.Result = menuItemToCreate;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return CreatedAtRoute("GetMenuItem", new { id = menuItemToCreate.Id }, _response);
+                }
+                else
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    return BadRequest(_response);
+                }   
+            }
+            catch(Exception e)
+            {
+                _response.IsSuccess = false;
+                _response.Errors.Add(e.ToString());
+            }
+
+            return _response;
+        }   
     }
 }
