70150a0bb4dbdb5f2dd65d71eeae880587e0db70
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-12 19:52:24 +0100

    Add migration to refactor IdentityUser table by adding name column

--- RESTaurantAPI/Data/ApplicationDbContext.cs
+++ RESTaurantAPI/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RESTaurantAPI.Models;
 
 namespace RESTaurantAPI.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
             
         }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
