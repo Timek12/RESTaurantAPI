322e42cc2dcd1c4aa5a930a28cc7901ebd14ad1a
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-13 23:55:24 +0100

    Add services for user manager and role manager using DI

---
+++ RESTaurantAPI/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RESTaurantAPI.Data;
+using RESTaurantAPI.Models;
+using RESTaurantAPI.Services;
+
+namespace RESTaurantAPI.Controllers
+{
+    [Route("api/Auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private string secretKey;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private ApiResponse _response;
+        public AuthController(ApplicationDbContext db, IConfiguration configuration, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:SecretKey");
+            _response = new ApiResponse();
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+    }
+}
+ 
