85314f961005963ab80648eae1d0b0cd38a21f16
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-13 23:31:35 +0100

    Add admin and user static strings

--- RESTaurantAPI/Utility/SD.cs
+++ RESTaurantAPI/Utility/SD.cs
@@ -3,5 +3,7 @@
     public static class SD
     {
         public const string SD_Storage_Container = "foodimages";
+        public const string SD_Role_Admin = "Admin";
+        public const string SD_Role_Customer = "Customer";
     }
 }
