dbc7df115edb9650534f357744fb74103fee0692
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-14 20:31:55 +0100

    Add api for testing authentication and authorization

---
+++ RESTaurantAPI/Controllers/AuthTestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RESTaurantAPI.Utility;
+
+namespace RESTaurantAPI.Controllers
+{
+    [Route("api/AuthTest")]
+    [ApiController]
+    public class AuthTestController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<string>> GetSomething()
+        {
+            return "You are authenticated.";
+        }
+
+        [HttpGet("{id:int}")]
+        [Authorize(Roles = SD.Role_Admin)]
+        public async Task<ActionResult<string>> GetSomething(int someIntValue)
+        {
+            return "You are authenticated with Role of Admin.";
+        }
+    }
+}
