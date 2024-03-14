72c89b51747e5b9d81b1e562a051218ef5906f67
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-14 20:30:36 +0100

    Rename 'SD' from property names

--- RESTaurantAPI/Utility/SD.cs
+++ RESTaurantAPI/Utility/SD.cs
@@ -2,8 +2,8 @@
 {
     public static class SD
     {
-        public const string SD_Storage_Container = "foodimages";
-        public const string SD_Role_Admin = "Admin";
-        public const string SD_Role_Customer = "Customer";
+        public const string Storage_Container = "foodimages";
+        public const string Role_Admin = "Admin";
+        public const string Role_Customer = "Customer";
     }
 }
