649fd40e9fd0c3ac90b90faa8706f84b82c3a7d7
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-14 23:00:54 +0100

    Add swagger configuration from documenation for debugging JWT Bearer token usin AuthTest controller

--- RESTaurantAPI/Program.cs
+++ RESTaurantAPI/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using RESTaurantAPI.Data;
 using RESTaurantAPI.Models;
 using RESTaurantAPI.Services;
@@ -45,7 +46,38 @@ builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Swagger documenation for adding JWT Bearer
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+    {
+        Description =
+        "JWT Authorization header using Bearer scheme. \n\n" +
+        "Enter 'Bearer' [space] and then your token in the input below. \n\n" +
+        "Example: \"Bearer a1b2c3d4\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = JwtBearerDefaults.AuthenticationScheme
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+        },
+        new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
