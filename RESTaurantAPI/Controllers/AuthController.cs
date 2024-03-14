5053967b81ff8cb5764d2bb51bd2d277b9551968
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-14 18:16:37 +0100

    Add register endpoint with creating new user, if roles do not exists, create admin and user roles

--- RESTaurantAPI/Controllers/AuthController.cs
+++ RESTaurantAPI/Controllers/AuthController.cs
@@ -3,11 +3,14 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using RESTaurantAPI.Data;
 using RESTaurantAPI.Models;
+using RESTaurantAPI.Models.Dto;
 using RESTaurantAPI.Services;
+using RESTaurantAPI.Utility;
+using System.Net;
 
 namespace RESTaurantAPI.Controllers
 {
-    [Route("api/Auth")]
+    [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
     {
@@ -24,6 +27,61 @@ namespace RESTaurantAPI.Controllers
             _userManager = userManager;
             _roleManager = roleManager;
         }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO model)
+        {
+            try
+            {
+                ApplicationUser userFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
+                if (userFromDb is not null)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.Errors.Add("User already exists");
+                    return BadRequest(_response);
+                }
+
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = model.UserName,
+                    Name = model.Name,
+                    Email = model.UserName,
+                    NormalizedEmail = model.UserName.ToUpper(),
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    if (!_roleManager.RoleExistsAsync(SD.SD_Role_Admin).GetAwaiter().GetResult())
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.SD_Role_Admin));
+                        await _roleManager.CreateAsync(new IdentityRole(SD.SD_Role_Customer));
+                    }
+
+                    if (model.Role.ToLower() == SD.SD_Role_Admin.ToLower())
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.SD_Role_Admin);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.SD_Role_Customer);
+                    }
+
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.IsSuccess = true;
+                    return Ok(_response);
+                }
+
+            }
+            catch (Exception e)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.Errors.Add("Error while registering user");
+            }
+
+            return BadRequest(_response);
+        }
     }
 }
- 
