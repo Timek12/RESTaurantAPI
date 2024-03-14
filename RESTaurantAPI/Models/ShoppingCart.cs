8c7a99204d5bb870f7757ff20eb7841a57a1c434
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-14 23:40:05 +0100

    Create a shopping cart model, mark stripe properties as not mapped

---
+++ RESTaurantAPI/Models/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RESTaurantAPI.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public double CartTotal { get; set; }
+        [NotMapped]
+        public ICollection<CartItem> CartItems { get; set; }
+        [NotMapped]
+        public string StripePaymentIntentId { get; set; }
+        [NotMapped]
+        public string ClientSecret { get; set; }
+    }
+}
