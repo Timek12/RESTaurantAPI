5a7d12d707b33d0fbca9a32b7ee2495b986f0f90
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-15 16:47:26 +0100

    Add api controller for functionalities related to shopping cart, add database and api response using DI

---
+++ RESTaurantAPI/Controllers/ShoppingCartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTaurantAPI.Data;
+using RESTaurantAPI.Models;
+
+namespace RESTaurantAPI.Controllers
+{
+    [Route("api/ShoppingCart")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        protected ApiResponse _response;
+        private readonly ApplicationDbContext _db;
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            _response = new();
+            _db = db;
+        }
+    }
+}
