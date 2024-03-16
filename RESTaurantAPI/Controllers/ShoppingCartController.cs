eecb34a613bbef5072253144185acd000f7a9974
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-16 18:40:13 +0100

    Add endpoint for adding new item into shopping cart or update existing item

--- RESTaurantAPI/Controllers/ShoppingCartController.cs
+++ RESTaurantAPI/Controllers/ShoppingCartController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RESTaurantAPI.Data;
 using RESTaurantAPI.Models;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
 
 namespace RESTaurantAPI.Controllers
 {
@@ -16,5 +19,85 @@ namespace RESTaurantAPI.Controllers
             _response = new();
             _db = db;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int menuItemId, int updateQuantityBy)
+        {
+            ShoppingCart? shoppingCart = _db.ShoppingCarts.Include(u => u.CartItems).FirstOrDefault(u => u.UserId == userId);
+            MenuItem? menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == menuItemId);
+
+            if (menuItem is null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+
+            if(shoppingCart is null && updateQuantityBy > 0)
+            {
+                // create a shopping cart & add cart item
+                ShoppingCart newCart = new()
+                {
+                    UserId = userId,
+                };
+
+                _db.ShoppingCarts.Add(newCart);
+                await _db.SaveChangesAsync();
+
+                CartItem newCartItem = new()
+                {
+                    MenuItemId = menuItemId,
+                    Quantity = updateQuantityBy,
+                    ShoppingCartId = newCart.Id,
+                    MenuItem = null,
+                };
+
+                _db.CartItems.Add(newCartItem);
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                // shopping cart exists, only add cart item
+                CartItem cartItemInCart = shoppingCart.CartItems.FirstOrDefault(u => u.MenuItemId == menuItemId);
+                if (cartItemInCart is null)
+                {
+                    // item does not exists in current cart
+                    CartItem newCartItem = new()
+                    {
+                        MenuItemId = menuItemId,
+                        Quantity = updateQuantityBy,
+                        ShoppingCartId = shoppingCart.Id,
+                        MenuItem = null,
+                    };
+
+                    _db.CartItems.Add(newCartItem);
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    // item already exists in the cart, only update quantity
+                    int newQuantity = cartItemInCart.Quantity + updateQuantityBy;
+                    if(updateQuantityBy == 0 || newQuantity <= 0)
+                    {
+                        // remove cart item from cart and if only item remove cart
+                        _db.CartItems.Remove(cartItemInCart);
+                        if(shoppingCart.CartItems.Count() == 1)
+                        {
+                            _db.ShoppingCarts.Remove(shoppingCart);
+                        }
+                        await _db.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        cartItemInCart.Quantity = newQuantity;
+                        await _db.SaveChangesAsync();
+                    }
+                }
+
+            }
+            return Ok(_response);
+        }
+
+        
     }
 }
