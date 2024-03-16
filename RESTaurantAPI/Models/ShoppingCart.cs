59f74bb8b353b1737992751df4e0b47b5740e900
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-16 23:57:51 +0100

    Change not mapped attribute from cart items to cart total property

--- RESTaurantAPI/Models/ShoppingCart.cs
+++ RESTaurantAPI/Models/ShoppingCart.cs
@@ -6,9 +6,11 @@ namespace RESTaurantAPI.Models
     {
         public int Id { get; set; }
         public string UserId { get; set; }
-        public double CartTotal { get; set; }
-        [NotMapped]
         public ICollection<CartItem> CartItems { get; set; }
+
+        [NotMapped]
+        public double CartTotal { get; set; }
+        
         [NotMapped]
         public string StripePaymentIntentId { get; set; }
         [NotMapped]
