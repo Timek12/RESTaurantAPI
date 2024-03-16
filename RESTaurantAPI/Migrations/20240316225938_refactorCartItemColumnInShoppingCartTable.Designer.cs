0589b8ab3a3d357fcf3091df4388f5abcf72036b
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-17 00:00:26 +0100

    Refactor cart item column in shopping cart table

--- RESTaurantAPI/Migrations/20240316225938_refactorCartItemColumnInShoppingCartTable.Designer.cs
+++ RESTaurantAPI/Migrations/20240316225938_refactorCartItemColumnInShoppingCartTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RESTaurantAPI.Data;
 
@@ -11,9 +12,11 @@ using RESTaurantAPI.Data;
 namespace RESTaurantAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240316225938_refactorCartItemColumnInShoppingCartTable")]
+    partial class refactorCartItemColumnInShoppingCartTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RESTaurantAPI/Migrations/20240316225938_refactorCartItemColumnInShoppingCartTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RESTaurantAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class refactorCartItemColumnInShoppingCartTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CartTotal",
+                table: "ShoppingCarts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ShoppingCartId",
+                table: "CartItems",
+                column: "ShoppingCartId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_ShoppingCarts_ShoppingCartId",
+                table: "CartItems",
+                column: "ShoppingCartId",
+                principalTable: "ShoppingCarts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_ShoppingCarts_ShoppingCartId",
+                table: "CartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_ShoppingCartId",
+                table: "CartItems");
+
+            migrationBuilder.AddColumn<double>(
+                name: "CartTotal",
+                table: "ShoppingCarts",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
