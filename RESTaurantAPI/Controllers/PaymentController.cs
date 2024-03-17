16b7f714756a822973b5075e6ec96ebc84cc32af
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-17 22:22:34 +0100

    Configure and create stripe payment intent

--- RESTaurantAPI/Controllers/PaymentController.cs
+++ RESTaurantAPI/Controllers/PaymentController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RESTaurantAPI.Data;
 using RESTaurantAPI.Models;
+using Stripe;
 using System.Net;
 
 namespace RESTaurantAPI.Controllers
@@ -37,7 +38,26 @@ namespace RESTaurantAPI.Controllers
 
             #region Create Payment Intent
 
+            StripeConfiguration.ApiKey = _configuration["StripeSettings:SecretKey"];
+            shoppingCart.CartTotal = shoppingCart.CartItems.Sum(u => u.Quantity * u.MenuItem.Price);
+
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = (int)(shoppingCart.CartTotal * 100),
+                Currency = "usd",
+                AutomaticPaymentMethods = new PaymentIntentAutomaticPaymentMethodsOptions
+                {
+                    Enabled = true,
+                },
+            };
+            var service = new PaymentIntentService();
+            PaymentIntent response = service.Create(options);
+
+            shoppingCart.StripePaymentIntentId = response.Id;
+            shoppingCart.ClientSecret = response.ClientSecret;
+
             #endregion
+
             _response.Result = shoppingCart;
             _response.StatusCode = HttpStatusCode.OK;
             return Ok(_response);
