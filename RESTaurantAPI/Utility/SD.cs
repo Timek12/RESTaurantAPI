2c4bf78829a2abe0b19e416759e4c94cf02a9865
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-17 21:21:41 +0100

    Add const strings for order status

--- RESTaurantAPI/Utility/SD.cs
+++ RESTaurantAPI/Utility/SD.cs
@@ -5,5 +5,12 @@
         public const string Storage_Container = "foodimages";
         public const string Role_Admin = "Admin";
         public const string Role_Customer = "Customer";
+
+        public const string status_Pending = "Pending";
+        public const string status_Confirmed = "Confirmed";
+        public const string status_beingCooked = "Being Cooked";
+        public const string status_readyForPickup = "Ready for Pickup";
+        public const string status_Completed = "Completed";
+        public const string status_Cancelled = "Cancelled";
     }
 }
